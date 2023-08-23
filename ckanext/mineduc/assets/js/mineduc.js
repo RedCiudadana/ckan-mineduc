35cba2e70950fe03d61d5b8afd9d7b682234e78a
CKAN <ckan@DESKTOP-3Q31QI4>
2023-08-23 16:01:31 -0600

    feat(general), update styles

--- ckanext/mineduc/assets/js/mineduc.js
+++ ckanext/mineduc/assets/js/mineduc.js
@@ -2,6 +2,7 @@
   $(document).ready(function(){
     $('.slick-carousel').slick({
       autoplay: true,
+      lazyLoad: 'ondemand',
       autoplaySpeed: 2000,
     });
     // $('.slick-carousel')
