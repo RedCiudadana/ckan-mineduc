4fdd3d16be8636b318827e1212f1c55c78fb9a09
CKAN <ckan@DESKTOP-3Q31QI4>
2023-08-29 15:42:29 -0600

    fix(home), disabled slide and fix width

--- ckanext/mineduc/assets/js/mineduc.js
+++ ckanext/mineduc/assets/js/mineduc.js
@@ -1,10 +1,10 @@
 (function () {
   $(document).ready(function(){
-    $('.slick-carousel').slick({
-      autoplay: true,
-      lazyLoad: 'ondemand',
-      autoplaySpeed: 2000,
-    });
+    // $('.slick-carousel').slick({
+    //   autoplay: true,
+    //   lazyLoad: 'ondemand',
+    //   autoplaySpeed: 2000,
+    // });
     // $('.slick-carousel')
     //   .children()
     //   .each(
